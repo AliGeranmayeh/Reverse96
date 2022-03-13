87769d3b1a1fd659682aaa4b60ba5bcc09c619bf
expensive77 <aligeranmayeh9797@gmail.com>
2022-03-13 12:57:25 +0330

    add login operator and update requirements

--- project/main/user/urls.py
+++ project/main/user/urls.py
@@ -4,5 +4,7 @@ from . import views
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-path('register', views.RegisterView.as_view(), name="register"),
+    path('register', views.RegisterView.as_view(), name="register"),
+    path('token', jwt_views.TokenObtainPairView.as_view(), name="login"),
+    path('token/refresh', jwt_views.TokenRefreshView.as_view(), name="refresh-token"),
 ]
