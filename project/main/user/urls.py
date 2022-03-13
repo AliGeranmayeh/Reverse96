f9ec7a13eb61a25b051e44b6d5b7a8d4134bc60d
expensive77 <aligeranmayeh9797@gmail.com>
2022-03-13 12:44:02 +0330

    add registration view and it's url

--- project/main/user/urls.py
+++ project/main/user/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from . import views
 from rest_framework_simplejwt import views as jwt_views
 
-urlpatterns = []
+urlpatterns = [
+path('register', views.RegisterView.as_view(), name="register"),
+]
--- project/main/user/views.py
+++ project/main/user/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializer import CustomUserSerializer
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
+from .models import CustomUser
+from rest_framework import permissions, status
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
 
-# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        user_data = serializer.data
+        user = CustomUser.objects.get(id=user_data.get('id'))
+        access_tk = str(AccessToken.for_user(user))
+        refresh_tk = str(RefreshToken.for_user(user))
+        return Response({"message": serializer.data,
+                         "access": access_tk,
+                         "refresh": refresh_tk},
+                         status=status.HTTP_201_CREATED)
