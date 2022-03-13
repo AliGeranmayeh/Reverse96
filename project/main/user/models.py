9150ecc9e4e5c659e348ddf897bbd8046261d5e0
expensive77 <aligeranmayeh9797@gmail.com>
2022-03-13 11:58:26 +0330

    create a custom user model and update gitignore file to ignore migration files

--- project/main/user/models.py
+++ project/main/user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=300)
+    username = models.CharField(max_length=300, unique=True)
+    email = models.CharField(max_length=400, unique=True)
+    password = models.CharField(max_length=100)
+    phone_number = models.IntegerField(unique=True)
+    address = models.TextField()
+    def __str__(self):
+        return self.username
