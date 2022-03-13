03af499a326f64d8a93d002b267b88b9d7febd20
expensive77 <aligeranmayeh9797@gmail.com>
2022-03-13 13:07:31 +0330

    change access and refresh token's life time

--- project/main/main/settings.py
+++ project/main/main/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'user.CustomUser'
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': True,
+}
+
