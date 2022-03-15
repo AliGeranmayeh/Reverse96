6ad2608119a436bb56f1cdcd42ac5472dd468ca5
expensive77 <aligeranmayeh9797@gmail.com>
2022-03-15 18:35:13 +0330

    add structure of email validation and add it's url

--- project/main/user/views.py
+++ project/main/user/views.py
@@ -15,7 +15,7 @@ from random import randint
 
 def randomNumber():
     seed(1)
-    value = randint(1000,9999)
+    value = randint(1000, 9999)
     return value
 
 class RegisterView(APIView):
@@ -36,6 +36,7 @@ class RegisterView(APIView):
                          "access": access_tk,
                          "refresh": refresh_tk},
                          status=status.HTTP_201_CREATED)
+
 class LoginView(APIView):
     def post(self, request):
         serializer = LoginSerializer(data=request.data, partial=True)
@@ -43,10 +44,14 @@ class LoginView(APIView):
         username = serializer.validated_data.get("username")
         password = serializer.validated_data.get("password")
         user = CustomUser.objects.filter(Q(username=username)|Q(email=username)).first()
-        if not user or not check_password(password, user.password):
-            return Response ({"message": "Invalid user"}, status=status.HTTP_404_NOT_FOUND)
+        if not user:
+            return Response({"message": "invalid username or email"}, status=status.HTTP_404_NOT_FOUND)
+        if not check_password(password, user.password):
+            return Response({"message": "wrong password"}, status=status.HTTP_404_NOT_FOUND)
         access_tk = str(AccessToken.for_user(user))
         refresh_tk = str(RefreshToken.for_user(user))
         return Response(data={"access": access_tk, "refresh": refresh_tk}, status=status.HTTP_200_OK)
 
-
+class EmailValidationView(APIView):
+    def get(self,request):
+        pass
