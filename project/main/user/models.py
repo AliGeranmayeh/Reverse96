9209df47b5ff98bcd09f5951c8acf0a05464cb7c
expensive77 <aligeranmayeh9797@gmail.com>
2022-03-15 20:23:00 +0330

    add is_valid flag to custom user model

--- project/main/user/models.py
+++ project/main/user/models.py
@@ -10,6 +10,7 @@ class CustomUser(AbstractUser):
     password = models.CharField(max_length=100)
     phone_number = models.IntegerField(unique=True)
     address = models.TextField()
+    is_active = models.BooleanField(default=False)
     def __str__(self):
         return self.username
 
