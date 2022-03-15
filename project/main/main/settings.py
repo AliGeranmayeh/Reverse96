48d08c97ddd3bd401c8092019010a2d63f084a24
expensive77 <aligeranmayeh9797@gmail.com>
2022-03-15 17:05:59 +0330

    add email settings

--- project/main/main/settings.py
+++ project/main/main/settings.py
@@ -133,3 +133,10 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'agamar.corp.iust@gmail.com'
+EMAIL_HOST_PASSWORD = "qcjbchsmwddxoirh"
