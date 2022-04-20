920f0f6c9ced1efd42d77469948a8f06bddc4121
expensive77 <aligeranmayeh9797@gmail.com>
2022-04-20 14:06:55 +0430

    add test set up file

---
+++ project/main/tests/test_setUp.py
@@ -0,0 +1,27 @@
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from faker import Faker
+
+
+class Test_SetUp(APITestCase):
+    
+
+    def setUp(self):
+        self.register_url = reverse("register")
+        self.fake = Faker()
+
+        self.user_data = {
+            "username": self.fake.email().split('@') ,
+            "email": self.fake.email(),
+            "password": "12345",
+            "address": self.fake.address(),
+            "name": self.fake.name(),
+            "phone_number": self.fake.phone_number(),
+            "is_active":"True"
+        }
+
+        return super().setUp()
+
+
+    def tearDown(self):
+        return super().tearDown()
