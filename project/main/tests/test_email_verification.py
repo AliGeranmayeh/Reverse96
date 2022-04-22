ee89df5c5621f82ae8951b8016b34d6b793b95ec
expensive77 <aligeranmayeh9797@gmail.com>
2022-04-22 15:50:21 +0430

    test that user can't activate her email twice with old code

--- project/main/tests/test_email_verification.py
+++ project/main/tests/test_email_verification.py
@@ -65,3 +65,22 @@ class TestEmailVerification(Test_SetUp):
         self.assertEqual(res.status_code, 404)
         self.assertEqual(res.data['message'],"Invalid credentials")
 
+
+    def test_user_cannot_verify_email_twice(self):
+        user = get_user_model().objects.create_user(
+            username=self.user_data['username'],
+            password=self.user_data['password'],
+            phone_number=self.user_data['phone_number'],
+            address=self.user_data['address'],
+            email=self.user_data['email'],
+            name=self.user_data['name'],
+            is_active=True
+        )
+        email_verification = EmailValidation.objects.create(email=self.user_data['email'],code=1234)
+        fake=Faker()
+        data = {
+            "email":self.user_data['email'],
+            "code":1234
+        }
+        res = self.client.post(self.email_verification_url,data,format='json')
+        self.assertEqual(res.status_code, 208)
