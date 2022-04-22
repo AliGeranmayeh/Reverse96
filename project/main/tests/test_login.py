a6f4e4bc3497eb14e01e3bfa76407e83507d0bae
expensive77 <aligeranmayeh9797@gmail.com>
2022-04-22 15:02:41 +0430

    test that user can't login without correct password

--- project/main/tests/test_login.py
+++ project/main/tests/test_login.py
@@ -81,3 +81,31 @@ class TestLogin(Test_SetUp):
 
         res = self.client.post(self.login_url,data2,format='json')
         self.assertEqual(res.status_code, 200)
+    
+
+    def test_user_cannot_login_with_wrong_password(self):
+        user = get_user_model().objects.create_user(
+            username=self.user_data['username'],
+            password=self.user_data['password'],
+            phone_number=self.user_data['phone_number'],
+            address=self.user_data['address'],
+            email=self.user_data['email'],
+            name=self.user_data['name'],
+            is_active=True
+        )
+
+        data = {
+            "username": self.user_data['username'],
+            "password":"896756",
+        }
+        data2 = {
+            "username": self.user_data['email'],
+            "password":"896756",
+        }
+        res = self.client.post(self.login_url,data,format='json')
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(res.data["message"],"wrong password")
+
+        res = self.client.post(self.login_url,data2,format='json')
+        self.assertEqual(res.status_code, 404) 
+        self.assertEqual(res.data["message"],"wrong password")
