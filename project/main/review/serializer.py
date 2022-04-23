2ca00316d67ac16c38ac8e32b4a3cedd383c08d8
mak1047 <mohsenakbari1378@gmail.com>
2022-04-23 21:58:25 +0430

    added location serializer

--- project/main/review/serializer.py
+++ project/main/review/serializer.py
@@ -3,11 +3,15 @@ from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
 
-from .models import places
+from .models import places,location
 
 
 class review_serializer(serializers.ModelSerializer):
     class Meta:
         model = places
-        fields = ['id', 'name', 'user', 'address', 'picture']
+        fields = ['id', 'name', 'user', 'address', 'picture', 'date_created']
+class location_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = location
+        fields = ['id', 'long', 'latt']
 
