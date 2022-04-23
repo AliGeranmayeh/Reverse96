15e1d06f7ae4fbf7187e96b3a87ca1f3af25b561
mak1047 <mohsenakbari1378@gmail.com>
2022-04-23 08:52:47 +0430

    added serializer.py and urls .py

--- project/main/review/views.py
+++ project/main/review/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializer import LoginSerializer, RegisterSerializer, EmailActivisionSerializer, RefreshTokenSerializer, PublicProfileSerializer
+from rest_framework.response import Response
+from .models import CustomUser,EmailValidation
+from rest_framework import permissions, status
+from rest_framework_simplejwt.tokens import RefreshToken, AccessToken
+from django.db.models import Q
+from django.conf import settings
+from django.core.mail import send_mail
+from rest_framework.generics import GenericAPIView
 
 # Create your views here.
