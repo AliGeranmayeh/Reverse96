c31fd9194aaeacfd7f06a368c9ce57c81997a3b8
mak1047 <mohsenakbari1378@gmail.com>
2022-04-23 22:00:58 +0430

    added location urls

--- project/main/review/urls.py
+++ project/main/review/urls.py
@@ -5,4 +5,6 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     path('review', views.user_review.as_view(), name="user_review"),
+    path('get_location', views.get_location_api.as_view(), name="add_location_api"),
+    path('add_location', views.add_location_api.as_view(), name="get_location_api"),
 ]
