c90c4eaea86c464a6e0caa2b14dbf1917b37e288
mak1047 <mohsenakbari1378@gmail.com>
2022-04-23 09:51:17 +0430

    created review serializer

--- project/main/review/serializer.py
+++ project/main/review/serializer.py
@@ -1,2 +1,10 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from .models import places, location
+
+
+class review_serializer(serializers.ModelSerializer):
+    class Meta:
+        model = places
+        fields = ['id', 'name', 'user', 'address', 'picture']
+
