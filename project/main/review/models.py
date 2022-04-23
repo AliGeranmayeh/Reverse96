de44476a1d66bae053e29441190bdb1e2fb2d13d
mak1047 <mohsenakbari1378@gmail.com>
2022-04-23 21:41:41 +0430

    add location model

--- project/main/review/models.py
+++ project/main/review/models.py
@@ -1,4 +1,5 @@
 from operator import mod
+from unicodedata import name
 from django.db import models
 from user.models import CustomUser
 # Create your models here.
@@ -8,8 +9,14 @@ class places(models.Model):
     address = models.TextField()
     picture = models.ImageField(null=True, blank=True, upload_to='media/profiles/', default='profiles/default.png')
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    location=models.ForeignKey(location,on_delete=models.CASCADE)
+    date_created=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.id
 
-#class location(models.Model):
-    #long
-    #latt
-    
+class location(models.Model):
+    id=models.BigAutoField(primary_key=True)
+    long=models.DecimalField(max_digits=9, decimal_places=6)
+    latt=models.DecimalField(max_digits=9, decimal_places=6)
+    def __str__(self):
+        return self.id    
