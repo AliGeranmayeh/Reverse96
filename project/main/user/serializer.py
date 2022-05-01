224c573000b844a87207d29b3f69b32285ff5ba1
expensive77 <aligeranmayeh9797@gmail.com>
2022-05-01 20:09:59 +0430

    add serializer for change password

--- project/main/user/serializer.py
+++ project/main/user/serializer.py
@@ -74,3 +74,27 @@ class UserEditProfileSerializer(serializers.ModelSerializer):
         def update(self, validated_data):
             instance = self.Meta.model(**validated_data)
             return instance
+class ChangePasswordSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+    old_password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('old_password', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+        return attrs
+
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError({"old_password": "Old password is not correct"})
+        return value
+
+    def update(self, instance, validated_data):
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
