fae8c0f89f658e4a1374773a93b636e663414faf
expensive77 <aligeranmayeh9797@gmail.com>
2022-05-04 18:06:12 +0430

    add serailizer for poll and comment view

--- project/main/review/serializer.py
+++ project/main/review/serializer.py
@@ -1,9 +1,9 @@
 from urllib import request
 from rest_framework import serializers
 from rest_framework_simplejwt.tokens import RefreshToken, TokenError
+from fluent_comments.models import FluentComment
 
-
-from .models import locations, places
+from .models import locations, places,Poll
 
 
 class review_serializer(serializers.ModelSerializer):
@@ -15,3 +15,32 @@ class location_serializer(serializers.ModelSerializer):
         model = locations
         fields = ['id', 'long', 'latt']
 
+
+
+class RecursiveField(serializers.Serializer):
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(
+            value,
+            context=self.context)
+        return serializer.data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    children = RecursiveField(many=True)
+
+    class Meta:
+        model = FluentComment
+        fields = ('comment', 'id', 'children',)
+
+
+class PollSerializer(serializers.Serializer):
+    comments = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Poll
+        fields = ('name', 'details', 'comments')
+
+    def get_comments(self, obj):
+        poll_comment = FluentComment.objects.filter(object_pk=obj.id, parent_id=None)
+        serializer = CommentSerializer(poll_comment, many=True)
+        return serializer.data
