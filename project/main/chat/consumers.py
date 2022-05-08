d22f256735d19d0e2b3c051965640319d46f3936
mak1047 <mohsenakbari1378@gmail.com>
2022-05-08 09:41:31 +0430

    added fetch message

---
+++ project/main/chat/consumers.py
@@ -0,0 +1,26 @@
+from asyncio.windows_events import NULL
+from django.contrib.auth import get_user_model
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+import json
+from django.db.models.lookups import IsNull
+
+from rest_framework.fields import NullBooleanField
+from .models import Message, Chat, Contact
+from .views import get_last_10_messages, get_reply_message, get_user_contact, get_current_chat,get_unseen_messages
+
+User = get_user_model()
+
+
+class ChatConsumer(WebsocketConsumer):
+
+    def fetch_messages(self, data):
+        messages = get_last_10_messages(data['chatId'])#,data['id'])
+        content = {
+            'command': 'messages',
+            'messages': self.messages_to_json(messages)
+        }
+        print(content)
+        return self.send_message(content)
+
+
