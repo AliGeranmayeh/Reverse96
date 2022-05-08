5749298685b32843e48e3f9e812ddf176b516662
mak1047 <mohsenakbari1378@gmail.com>
2022-05-08 09:48:46 +0430

    added get reply and current contact and  unseen messages

--- project/main/chat/views.py
+++ project/main/chat/views.py
@@ -10,6 +10,19 @@ def get_last_10_messages(chatId, Mid):
     chat = get_object_or_404(Chat, id=chatId)
     return chat.messages.order_by('-timestamp').all()#[:Mid]
 
+def get_unseen_messages(chatId, Mfrom):
+    chat = get_object_or_404(Chat, id=chatId)
+    return chat.messages.filter(Q(flag=False) & Q(contact=Mfrom)).order_by('-timestamp')
+
+
+def get_user_contact(username):
+    user = get_object_or_404(User, username=username)
+    return get_object_or_404(Contact, user=user)
+
+def get_reply_message(chatId,Mid):
+    chat = get_object_or_404(Chat, id=chatId)
+    msg =chat.messages.get(id=Mid)
+    return msg
 
 def get_current_chat(chatId):
     return get_object_or_404(Chat, id=chatId)
