4b7bd4dd36e3daaecf3ddc6984db88e96c809862
mak1047 <mohsenakbari1378@gmail.com>
2022-05-08 09:50:45 +0430

    added ChatListView

---
+++ project/main/chat/api/views.py
@@ -0,0 +1,29 @@
+from django.contrib.auth import get_user_model
+from rest_framework import permissions
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    CreateAPIView,
+    DestroyAPIView,
+    UpdateAPIView
+)
+from chat.models import Chat, Contact
+from chat.views import get_user_contact
+from .serializers import ChatSerializer
+
+User = get_user_model()
+
+
+class ChatListView(ListAPIView):
+    serializer_class = ChatSerializer
+    permission_classes = (permissions.AllowAny, )
+
+    def get_queryset(self):
+        queryset = Chat.objects.all()
+        username = self.request.query_params.get('username', None)
+        if username is not None:
+            contact = get_user_contact(username)
+            queryset = contact.chats.all()
+        return queryset
+
+
