32258f24075ed0ea27a143517728bf38e190e9a1
mak1047 <mohsenakbari1378@gmail.com>
2022-05-08 09:43:30 +0430

    added new messages

--- project/main/chat/consumers.py
+++ project/main/chat/consumers.py
@@ -32,3 +32,25 @@ class ChatConsumer(WebsocketConsumer):
         }
         print(content)
         return    self.send_message(content)
+
+    def new_message(self, data):
+        user_contact = get_user_contact(data['from'])
+        if 'reply' in data:
+            message = Message.objects.create(
+            contact=user_contact,
+            reply=get_reply_message(data['chatId'],data['reply']),
+            content=data['message'])
+        else:
+            message = Message.objects.create(
+            contact=user_contact,
+            content=data['message'])
+        current_chat = get_current_chat(data['chatId'])
+        current_chat.messages.add(message)
+        current_chat.save()
+        content = {
+            'command': 'new_message',
+            'message': self.message_to_json(message)
+        }
+        return self.send_chat_message(content)
+
+
