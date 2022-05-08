8e176008931219fa364ecbcdc65ad8deab482702
mak1047 <mohsenakbari1378@gmail.com>
2022-05-08 09:48:18 +0430

    added get last messages and get current chat

--- project/main/chat/views.py
+++ project/main/chat/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_object_or_404
+from .models import Chat, Contact, Message
+from django.db.models import Q
 
-# Create your views here.
+User = get_user_model()
+
+
+def get_last_10_messages(chatId, Mid):
+    chat = get_object_or_404(Chat, id=chatId)
+    return chat.messages.order_by('-timestamp').all()#[:Mid]
+
+
+def get_current_chat(chatId):
+    return get_object_or_404(Chat, id=chatId)
