1085721cc161f5188e7a17e7edd0e553e5fe85ac
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-05-25 17:05:44 +0430

    add categories for places

--- project/main/review/models.py
+++ project/main/review/models.py
@@ -1,6 +1,7 @@
-
 from django.db import models
 from user.models import CustomUser
+
+
 # Create your models here.
 class locations(models.Model):
     id=models.BigAutoField(primary_key=True)
@@ -13,6 +14,26 @@ class locations(models.Model):
         return str(self.id)
 
 class review(models.Model):
+    place_categories=(
+        ("1", "Park"),
+        ("2", "Cinema"),
+        ("3", "Restaurant"),
+        ("4", "Stadium"),
+        ("5", "Cafe"),
+        ("6", "University"),
+        ("7", "School"),
+        ("8", "Street"),
+        ("9", "Mall"),
+        ("10", "Library"),
+        ("11", "Airport"),
+        ("12", "Train station"),
+        ("13", "Subway station"),
+        ("14", "Bus station"),
+        ("15", "Buffet"),
+        ("16", "Store"),
+        ("17", "Mosque")
+    )
+
     title=models.CharField(max_length=300)
     id=models.BigAutoField(unique=True, primary_key=True)
     text = models.TextField()
@@ -20,6 +41,7 @@ class review(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     location=models.ForeignKey(locations,on_delete=models.CASCADE)
     date_created=models.DateTimeField(auto_now_add=True)
+    place_category = models.CharField(max_length=200, choices=place_categories, null=True)
     liked_by=models.ManyToManyField(CustomUser,related_name="liked_by")
     def __str__(self):
         return str(self.id)
