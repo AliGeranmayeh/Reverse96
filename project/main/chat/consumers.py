8e37e41c75cb90016ce4ae0234846984664f1475
mak1047 <mohsenakbari1378@gmail.com>
2022-05-30 08:56:15 +0430

    fixed old contact.use.user name bug

--- project/main/chat/consumers.py
+++ project/main/chat/consumers.py
@@ -104,7 +104,7 @@ class ChatConsumer(WebsocketConsumer):
         else:
             return {
                 'id': message.id,
-                'author': message.contact.user.username,
+                'author': message.contact.username,
                 'content': message.content,
                 'timestamp': str(message.timestamp),
                 'flag':message.flag,
