5c59077605cfc2c10e62d29cce3e4d79bd03137e
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-05-31 21:05:31 +0430

    test category response objects are correct

--- project/main/tests/test_category.py
+++ project/main/tests/test_category.py
@@ -17,3 +17,27 @@ class TestCategory(Test_SetUp):
         #import pdb; pdb.set_trace()
         self.assertEqual(res.status_code, 200)
 
+    def test_category_response_objects_are_correct(self):
+        location = locations.objects.create(
+            name= self.location_data["name"],
+            long=self.location_data["long"],
+            latt=self.location_data["latt"],
+            place_category=self.location_data["place_category"]
+        )
+        data = {
+            "coordinates": [1,1,5,5],
+            "place_category": 3
+        }
+        res = self.client.post(self.category_url, data, format='json')
+        name = list(res.data['message'][0].items())[1][1]
+        long = list(res.data['message'][0].items())[3][1]
+        latt = list(res.data['message'][0].items())[4][1]
+        place_category = list(res.data['message'][0].items())[7][1]
+        #import pdb; pdb.set_trace()
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(name, f'{self.location_data["name"]}')
+        self.assertEqual(long, f'{self.location_data["long"]}00000000000000')
+        self.assertEqual(latt, f'{self.location_data["latt"]}00000000000000')
+        self.assertEqual(place_category, self.location_data["place_category"])
+
+
