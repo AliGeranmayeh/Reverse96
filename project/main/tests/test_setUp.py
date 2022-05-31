95d21e51674d511abced33fd495a5044f055cf4d
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-05-31 20:21:05 +0430

    add category url to test set up

--- project/main/tests/test_setUp.py
+++ project/main/tests/test_setUp.py
@@ -9,6 +9,7 @@ class Test_SetUp(APITestCase):
     def setUp(self):
         self.fake = Faker()
         self.register_url= reverse('register')
+        self.category_url = reverse('limited-area-category')
         self.login_url= reverse('login')
         self.email_verification_url= reverse('email-activision')
         self.user_data = {
