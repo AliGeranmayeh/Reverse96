5b1625173f50c3fafc02398c9e4c1288d19c2e0b
mak1047 <mohsenakbari1378@gmail.com>
2022-05-31 14:07:48 +0430

    added notification consumer

---
+++ project/main/notification/consumers.py
@@ -0,0 +1,72 @@
+import json
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+
+from user.models import CustomUser
+from .models import notification
+
+class NotificationConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = 'notification_%s' % self.room_name
+
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
+        user=CustomUser.objects.get(username=self.room_name)
+        notifs=notification.objects.filter(to_user=user)
+
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'notification.message',
+                'message': self.messages_to_json(notifs)
+            }
+        )
+
+    def disconnect(self, close_code):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    # Receive message from WebSocket
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        if not(text_data_json['received_id'] is None):
+            recieved_id=text_data_json['received_id']
+            for i in recieved_id:
+                notif=notification.objects.get(id=i)
+                notif.delete()
+
+        message = text_data_json['message']
+        print(message)
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'notification.message',
+                'message': message
+            }
+        )
+    def messages_to_json(self, notifs):
+        result = []
+        for notif in notifs:
+            result.append({
+            "from":notif.from_user.username,
+            "notif_id":notif.id
+            })
+        return result   
+
+    # Receive message from room group
+    def notification_message(self, event):
+        message = event['message']
+        # Send message to WebSocket
+        self.send(text_data=json.dumps({
+            'message': message
+        }))
+
