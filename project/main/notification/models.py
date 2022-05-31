d252969e5d10dae29b80dd0c1329536bfab4c95b
mak1047 <mohsenakbari1378@gmail.com>
2022-05-31 14:08:12 +0430

    added notification model

---
+++ project/main/notification/models.py
@@ -0,0 +1,15 @@
+import re
+from django.db import models
+from user.models import CustomUser
+# Create your models here.
+class notification(models.Model):
+    id=models.BigAutoField(primary_key=True)
+    to_user=models.ForeignKey(CustomUser,related_name='notifee',on_delete=models.CASCADE)
+    from_user=models.ForeignKey(CustomUser,related_name='notifer',on_delete=models.CASCADE)
+    content=models.CharField(max_length=50,null=True,blank=True)
+    created=models.DateTimeField(auto_now_add=True)
+    is_recieved=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.id
+        
