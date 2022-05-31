9d05a65a04d6c9e5154a6e055c18dd0ecd3a9f43
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-05-31 20:45:05 +0430

    test category response with 200

--- project/main/tests/test_category.py
+++ project/main/tests/test_category.py
@@ -1,4 +1,19 @@
 from .test_setUp import Test_SetUp
+from review.models import locations
 
 class TestCategory(Test_SetUp):
-    pass
+    def test_category_response_with_200(self):
+        location = locations.objects.create(
+            name= self.location_data["name"],
+            long=self.location_data["long"],
+            latt=self.location_data["latt"],
+            place_category=self.location_data["place_category"]
+        )
+        data = {
+            "coordinates": [1,1,5,5],
+            "place_category": 3
+        }
+        res = self.client.post(self.category_url, data, format='json')
+        #import pdb; pdb.set_trace()
+        self.assertEqual(res.status_code, 200)
+
