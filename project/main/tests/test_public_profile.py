241fa5ce299b16ed9a6919bac4dcadc739251601
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-05-31 14:14:25 +0430

    test user cant see non existing user

--- project/main/tests/test_public_profile.py
+++ project/main/tests/test_public_profile.py
@@ -35,3 +35,25 @@ class TestPublicProfileView(Test_SetUp):
         response = self.client.get(public_profile_url, format="json")
         #import pdb; pdb.set_trace()
         self.assertEqual(response.status_code, 200)
+
+    def test_user_cant_see_non_existing_user(self):
+        user = get_user_model().objects.create_user(
+            username=self.user_data['username'],
+            password=self.user_data['password'],
+            phone_number=self.user_data['phone_number'],
+            address=self.user_data['address'],
+            email=self.user_data['email'],
+            name=self.user_data['name'],
+            is_active=True
+        )
+        data = {
+            "username": self.user_data['username'],
+            "password": self.user_data['password'],
+        }
+        res = self.client.post(self.login_url, data, format='json')
+
+        public_profile_url = f"/api/public-profile/dfjksf"
+        header = self.client.credentials(HTTP_AUTHORIZATION='Bearer %s' % res.json()['access'])
+        response = self.client.get(public_profile_url, format="json")
+        # import pdb; pdb.set_trace()
+        self.assertEqual(response.status_code, 404)
