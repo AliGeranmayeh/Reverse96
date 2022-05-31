3ae511183b03dcb47792a8fa1d854270b291c2cc
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-05-31 13:54:47 +0430

    add second user for public profile checking

--- project/main/tests/test_public_profile.py
+++ project/main/tests/test_public_profile.py
@@ -14,16 +14,24 @@ class TestPublicProfileView(Test_SetUp):
             name=self.user_data['name'],
             is_active=True
         )
+        user2 = get_user_model().objects.create_user(
+            username=self.user_data2['username'],
+            password=self.user_data2['password'],
+            phone_number=self.user_data2['phone_number'],
+            address=self.user_data2['address'],
+            email=self.user_data2['email'],
+            name=self.user_data2['name'],
+            is_active=True
+        )
+
         data = {
             "username": self.user_data['username'],
             "password": self.user_data['password'],
         }
         res = self.client.post(self.login_url, data, format='json')
 
-        public_profile_url = f"/api/public-profile/{user.username}"
+        public_profile_url = f"/api/public-profile/{user2.username}"
         header =self.client.credentials(HTTP_AUTHORIZATION='Bearer %s' % res.json()['access'])
         response = self.client.get(public_profile_url, format="json")
         #import pdb; pdb.set_trace()
         self.assertEqual(response.status_code, 200)
-    
-        
