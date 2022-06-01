d76861e797188c883140a5719cef306b4bc11491
mak1047 <mohsenakbari1378@gmail.com>
2022-06-01 20:02:04 +0430

    ok

--- project/main/notification/consumers.py
+++ project/main/notification/consumers.py
@@ -9,7 +9,7 @@ class NotificationConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'notification_%s' % self.room_name
-
+        print(self.room_group_name)
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
@@ -69,6 +69,7 @@ class NotificationConsumer(WebsocketConsumer):
     def notification_message(self, event):
         message = event['message']
         # Send message to WebSocket
+        print(1)
         self.send(text_data=json.dumps({
             'message': message
         }))
--- project/main/notification/signals.py
+++ project/main/notification/signals.py
@@ -1,30 +1,35 @@
 
+from dis import Instruction
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import notification
+from django.db.models.query import QuerySet
+i=0
+@receiver(post_save, sender=notification,dispatch_uid="my_unique_identifier")
+def send_notif(sender,instance,raw,created, **kwargs):
 
+    if i==0:
+        i=i+1
+        print(instance)
+        current_user = instance.to_user # Getting current user
+        
+        channel_layer = get_channel_layer()
+        data = {
+                "from":instance.from_user.username,
+                "notif_id":instance.id,
+                "notif":instance.content
+                }
 
-@receiver(post_save, sender=notification)
-def send_notif(sender,instance,created, **kwargs):
-
-
-    current_user = instance.to_user # Getting current user
-
-    channel_layer = get_channel_layer()
-    data = {
-            "from":instance.from_user.username,
-            "notif_id":instance.id,
-            "notif":instance.content
+        group_name="notification_%s" % current_user.username
+        async_to_sync(channel_layer.group_send)(
+            group_name,  # Group Name, Should always be string
+            {
+                "type": 'notification.message',   # Custom Function written in the consumers.py
+                "message":{'messages':data,
+                            'command':'post_connect'
+                        }   
             }
-    group_name="notification_%s" % current_user.username
-    async_to_sync(channel_layer.group_send)(
-        group_name,  # Group Name, Should always be string
-        {
-            "type": "notification.message",   # Custom Function written in the consumers.py
-            "message":{'messages':data,
-                        'command':'post_connect'
-                    }   
-        }
-    )
+        )
+
