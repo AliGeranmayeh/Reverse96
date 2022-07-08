345abe88d33883018c787432953e01e5640c6f60
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-07-08 11:54:58 +0430

    add structure of review search view

--- project/main/review/views.py
+++ project/main/review/views.py
@@ -269,3 +269,8 @@ class SearchLocationView(APIView):
         find_locations = locations.objects.filter(name__icontains=pk)
         loc_serializer = location_review_serializer(find_locations, many=True)
         return Response(loc_serializer.data, status=status.HTTP_302_FOUND)
+
+
+class SearchReviewView(APIView):
+    def get(self, request, pk):
+        pass
