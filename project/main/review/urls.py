0c2160c6bf58f6150a2096c942e9cd960ca513fe
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-07-08 11:44:54 +0430

    add user search url

--- project/main/review/urls.py
+++ project/main/review/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path('rates/<str:pk>', views.ViewRateView.as_view(), name="all-rates"),
     path('add_user_like/<str:pk>', views.RateView.as_view(), name="add-rate"),
     path('category', views.Category.as_view(), name="limited-area-category"),
-    path('search/<slug:pk>',views.SearchView.as_view(),name= "search")
+    path('search/<slug:pk>',views.SearchView.as_view(),name= "search"),
+    path('u_search/<slug:pk>',views.SearchUserView.as_view(),name= "search_user")
 
 ]
