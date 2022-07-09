3ce5a41dad827b73f0a55b96632354041c7e8b54
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-07-09 18:21:37 +0430

    add  location_search_show_matched_query test

--- project/main/tests/test_search_views.py
+++ project/main/tests/test_search_views.py
@@ -2,6 +2,7 @@ from .test_setUp import Test_SetUp
 from django.contrib.auth import get_user_model
 from faker import Faker
 from random import randint
+from review.models import locations,review
 
 class TestSearch(Test_SetUp):
     def test_search_cant_show_unmatching_query(self):
@@ -31,4 +32,38 @@ class TestSearch(Test_SetUp):
         response = self.client.get(search_url, format="json")
         #import pdb; pdb.set_trace()
         self.assertEqual(response.status_code, 404)
+
+
+    def test_location_search_show_matched_query(self):
+        location = locations.objects.create(
+            name=self.location_data["name"],
+            long=self.location_data["long"],
+            latt=self.location_data["latt"],
+            place_category=self.location_data["place_category"]
+        )
+        location1 = locations.objects.create(
+            name=self.location_data1["name"],
+            long=self.location_data1["long"],
+            latt=self.location_data1["latt"],
+            place_category=self.location_data1["place_category"]
+        )
+
+        location2 = locations.objects.create(
+            name=self.location_data2["name"],
+            long=self.location_data2["long"],
+            latt=self.location_data2["latt"],
+            place_category=self.location_data2["place_category"]
+        )
+        test_search = 'az'
+        search_url = f"/api/l_search/{test_search}"
+        response = self.client.get(search_url, format="json")
+        #import pdb; pdb.set_trace()
+        self.assertEqual(response.status_code, 302)
+        name = list(response.data[0].items())[1][1]
+        long = list(response.data[0].items())[3][1]
+        latt = list(response.data[0].items())[4][1]
+        self.assertEqual(name, f'{self.location_data["name"]}')
+        self.assertEqual(long, f'{self.location_data["long"]}.000000000000000')
+        self.assertEqual(latt, f'{self.location_data["latt"]}.000000000000000')
+
 
