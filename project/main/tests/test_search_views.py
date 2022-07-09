11180a98b472a1361e0b88b6f5a0a13e07b16baa
expensive77 <51305499+expensive77@users.noreply.github.com>
2022-07-09 18:39:57 +0430

    add user_search_show_matched_query test

--- project/main/tests/test_search_views.py
+++ project/main/tests/test_search_views.py
@@ -66,4 +66,39 @@ class TestSearch(Test_SetUp):
         self.assertEqual(long, f'{self.location_data["long"]}.000000000000000')
         self.assertEqual(latt, f'{self.location_data["latt"]}.000000000000000')
 
+    def test_user_search_show_matched_query(self):
+        user = get_user_model().objects.create_user(
+            username="mohsen",
+            password=self.user_data['password'],
+            phone_number=self.user_data['phone_number'],
+            address=self.user_data['address'],
+            email=self.user_data['email'],
+            name=self.user_data['name'],
+            is_active=self.user_data['is_active']
+        )
+
+        user = get_user_model().objects.create_user(
+            username= "expensive",
+            password=self.user_data2['password'],
+            phone_number=self.user_data2['phone_number'],
+            address=self.user_data2['address'],
+            email=self.user_data2['email'],
+            name=self.user_data2['name'],
+            is_active=self.user_data2['is_active']
+        )
+        test_search = 'ex'
+        search_url = f"/api/u_search/{test_search}"
+        response = self.client.get(search_url, format="json")
+        #import pdb; pdb.set_trace()
+        name = list(response.data[0].items())[2][1]
+        username = list(response.data[0].items())[0][1]
+        email = list(response.data[0].items())[1][1]
+        address= list(response.data[0].items())[3][1]
+        phone = list(response.data[0].items())[5][1]
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(username, 'expensive')
+        self.assertEqual(name, f'{self.user_data2["name"]}')
+        self.assertEqual(email, f'{self.user_data2["email"]}')
+        self.assertEqual(address, f'{self.user_data2["address"]}')
+        self.assertEqual(phone, f'{self.user_data2["phone_number"]}')
 
