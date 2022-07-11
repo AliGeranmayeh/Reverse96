98209ada3be6f6ba3e71887d18dffa9021bcc286
mak1047 <mohsenakbari1378@gmail.com>
2022-07-11 20:05:22 +0430

    ok

--- project/main/notification/views.py
+++ project/main/notification/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import notification
-import Q
+from django.db.models import Q
 # Create your views here.
 def notif(T_user,user,content):
     notification.objects.create(to_user=T_user,from_user=user,content=content)
